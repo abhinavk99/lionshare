37078effdaac3addbf34d0b2d7bc0f624978b103
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-04-22 18:41:14 -0500

    Modify version in setup

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='lionshare',
     packages=['lionshare'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='Python wrapper for the Lionshare API',
     author='Abhinav Kasamsetty',
     author_email='abhinavkasamsetty@gmail.com',
     url='https://github.com/abhinavk99/lionshare',
-    keywords=['lionshare', 'cryptocurrency', 'api', 'wrapper'],
-    install_requires=['requests', 'pytest'],
-    tests=[
-        'tests/test_lionshare.py'
-    ]
+    keywords=['lionshare', 'cryptocurrency', 'api', 'wrapper']
 )
